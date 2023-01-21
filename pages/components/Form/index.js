45f9568463ebd1e0d277400d505c370858f86e58
Astrid Ottmann <astrid.ottmann@gmail.com>
2023-01-21 20:08:08 +0100

    sort-function and avoid douples

--- pages/components/Form/index.js
+++ pages/components/Form/index.js
@@ -1,26 +1,34 @@
+import styled from "styled-components";
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
+`;
+
 export default function Form({ onAddTranslations }) {
   function handleSubmit(event) {
     event.preventDefault();
 
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
-    console.log(data);
-    event.target.reset();
+    console.log("Form", data);
+
     onAddTranslations({ word: data.word, translated: data.translated });
+
+    event.target.reset();
+    event.target.elements.word.focus();
   }
+
   return (
     <>
-      <form onSubmit={(event) => handleSubmit(event)}>
-        <label htmlFor="word">
-          enter word
-          <input id="word" name="word" />
-        </label>
-        <label htmlFor="translated">
-          get translation
-          <input id="translated" name="translated" />
-        </label>
+      <StyledForm onSubmit={(event) => handleSubmit(event)}>
+        <label htmlFor="word">Enter word</label>
+        <input id="word" name="word" />
+        <label htmlFor="translated">Enter translation</label>
+        <input id="translated" name="translated" />
         <button type="submit">Submit</button>
-      </form>
+      </StyledForm>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,57 @@
 import Header from "./components/Header";
 import Form from "./components/Form";
-import { useState } from "react";
+import { startTransition, useState } from "react";
 import { nanoid } from "nanoid";
+import styled from "styled-components";
 
 const initialTranslations = [
   { id: 1, word: "verjaardag", translated: "Geburtstag" },
   { id: 2, word: "uitnodigen", translated: "einladen" },
   { id: 3, word: "verrassing", translated: "Überraschung" },
-  { id: 4, word: "schattig", tanslated: "niedlich" },
+  { id: 4, word: "schattig", translated: "niedlich" },
+  { id: 5, word: "moe", translated: "müde" },
 ];
+
+const StyledEntry = styled.div`
+  border: 1px dashed black;
+  border-radius: 10px;
+  margin: 1em;
+  padding: 0.5em;
+`;
 
 export default function HomePage() {
   const [translations, setTranslations] = useState(initialTranslations);
-  console.log(translations);
+  console.log("hier", translations);
+
+  initialTranslations.sort((a, b) => {
+    const wordA = a.word.toLowerCase();
+    const wordB = b.word.toLowerCase();
+    if (wordA < wordB) {
+      return -1;
+    }
+    if (wordA > wordB) {
+      return 1;
+    }
+    return 0;
+  });
 
   function handleAddTranslations(newTranslation) {
-    setTranslations([{ id: nanoid(), ...newTranslation }, ...translations]);
+    const newTranslations = translations
+      .slice()
+      .filter((translation) => translation.word !== newTranslation.word)
+      .sort((a, b) => {
+        const wordA = a.word.toLowerCase();
+        const wordB = b.word.toLowerCase();
+        if (wordA < wordB) {
+          return -1;
+        }
+        if (wordA > wordB) {
+          return 1;
+        }
+        return 0;
+      });
+    console.log("sort", newTranslations);
+    setTranslations([{ id: nanoid(), ...newTranslation }, ...newTranslations]);
   }
 
   return (
@@ -25,10 +61,10 @@ export default function HomePage() {
         <h1>Add word</h1>
         <Form onAddTranslations={handleAddTranslations} />
         {translations.map((translation) => (
-          <div key={translation.id}>
+          <StyledEntry key={translation.id}>
             <p>{translation.word}</p>
             <small>{translation.translated}</small>
-          </div>
+          </StyledEntry>
         ))}
       </main>
     </>
