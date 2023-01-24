3697501ac8172bed56db6e12403ae3b5423ff5ea
Astrid Ottmann <astrid.ottmann@gmail.com>
2023-01-24 20:41:25 +0100

    highlighting current page

--- components/MainNavigation/index.js
+++ components/MainNavigation/index.js
@@ -1,18 +1,32 @@
 import Link from "next/link";
 import StyledNav from "./StyledNavigation";
 import SVGIcon from "../Icons/SVGIcon";
+import { useRouter } from "next/router";
 
-export default function MainNavigation() {
+const MainNavigation = () => {
+  const router = useRouter();
+  const currentRoute = router.pathname;
+
   return (
     <>
       <StyledNav>
         <Link href="/">
-          <SVGIcon variant="home" width="3rem" color="whitesmoke" />
+          {currentRoute === "/" ? (
+            <SVGIcon variant="home" width="3rem" color="#F27405" />
+          ) : (
+            <SVGIcon variant="home" width="3rem" color="whitesmoke" />
+          )}
         </Link>
         <Link href="/words">
-          <SVGIcon variant="words" width="3rem" color="whitesmoke" />
+          {currentRoute === "/words" ? (
+            <SVGIcon variant="words" width="3rem" color="#F27405" />
+          ) : (
+            <SVGIcon variant="words" width="3rem" color="whitesmoke" />
+          )}
         </Link>
       </StyledNav>
     </>
   );
-}
+};
+
+export default MainNavigation;
